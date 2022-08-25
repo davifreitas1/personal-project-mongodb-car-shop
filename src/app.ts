868b5a25ce0deb4398e2c79ce322e2093461ad78
unknown <davivarejao@gmail.com>
2022-08-25 15:25:57 -0300

    feat: cars route and her implemetation

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import express from 'express';
+import carsRouter from './routes/CarsRoute';
 
 const app = express();
+app.use(express.json());
+app.use('/cars', carsRouter);
 
 export default app;
---
+++ src/routes/CarsRoute.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import CarsController from '../controllers/CarsController';
+import CarsValidation from '../middlewares/CarsValidation';
+import CarsModel from '../models/CarsModel';
+import CarsService from '../services/CarsService';
+
+const carsRouter = Router();
+const model = new CarsModel();
+const service = new CarsService(model);
+const controller = new CarsController(service);
+const middleware = new CarsValidation();
+
+carsRouter.post('/', middleware.validate, controller.create);
+
+export default carsRouter;
